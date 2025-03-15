cdcb7a1b2de2cc9fc7fa5f1686381c4939fa7cf0
Markus Wahlman <wahlman2002@gmail.com>
2025-03-15 17:30:04 +0200

    Fix linting

--- src/chart.py
+++ src/chart.py
@@ -1,9 +1,9 @@
 class Chart:
-    def __init__(self, chartData, onNotFound):
-        self.chartData = chartData
-        self.onNotFound = onNotFound
+    def __init__(self, chart_data, on_not_found):
+        self.chart_data = chart_data
+        self.on_not_found = on_not_found
 
-    def __call__(self, dealerCard, playerHand):
-        return self.chartData.get(
-            dealerCard, {}).get(
-            playerHand, self.onNotFound)
+    def __call__(self, dealer_card, player_hand):
+        return self.chart_data.get(
+            dealer_card, {}).get(
+            player_hand, self.on_not_found)
