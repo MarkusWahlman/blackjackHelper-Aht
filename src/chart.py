2205f7763af9f92067a2bea7f84fef52503be084
Markus Wahlman <wahlman2002@gmail.com>
2025-03-16 16:15:14 +0200

    Add get_chart_data to Chart

--- src/chart.py
+++ src/chart.py
@@ -2,11 +2,14 @@ class Chart:
     def __init__(self, chart_data, on_not_found=""):
         self.chart_data = chart_data
         self.on_not_found = on_not_found
-    
-    def set_on_not_found(self, on_not_found):
+
+    def set_on_not_found(self, on_not_found: str):
         self.on_not_found = on_not_found
 
-    def __call__(self, dealer_card, player_hand):
+    def get_chart_data(self):
+        return self.chart_data
+
+    def __call__(self, dealer_card, player_hand: list[str]):
         return self.chart_data.get(
             dealer_card, {}).get(
             player_hand, self.on_not_found)
--- src/tests/chart_test.py
+++ src/tests/chart_test.py
@@ -4,24 +4,28 @@ from chart import Chart
 
 class TestChart(unittest.TestCase):
     def setUp(self):
-        self.normalChart = Chart({
-            "1": {"1": "H", "2": "S"},
-            "2": {"1": "D", "2": "H"},
-            "3": {"1": "S", "2": "D"},
-        },
-            "404")
+        self.chart_data = {
+            "1": {"1": "1-1", "2": "1-2"},
+            "2": {"1": "2-1", "2": "2-2"},
+            "3": {"1": "3-1", "2": "3-2"},
+        }
+        self.normal_chart = Chart(self.chart_data, "404")
 
     def test_finds_right_values_from_chart(self):
-        chartValue1 = self.normalChart("3", "2")
-        chartValue2 = self.normalChart("1", "2")
-        self.assertEqual(chartValue1, "D")
-        self.assertEqual(chartValue2, "S")
+        chart_value1 = self.normal_chart("3", "2")
+        chart_value2 = self.normal_chart("1", "2")
+        self.assertEqual(chart_value1, "3-2")
+        self.assertEqual(chart_value2, "1-2")
 
     def test_returns_set_value_if_not_found(self):
-        chartValue = self.normalChart("10", "10")
-        self.assertEqual(chartValue, "404")
+        chart_value = self.normal_chart("10", "10")
+        self.assertEqual(chart_value, "404")
 
     def test_can_set_return_value(self):
-        self.normalChart.set_on_not_found("not_found")
-        chartValue = self.normalChart("10", "10")
-        self.assertEqual(chartValue, "not_found")
+        self.normal_chart.set_on_not_found("not_found")
+        chart_value = self.normal_chart("10", "10")
+        self.assertEqual(chart_value, "not_found")
+
+    def test_get_chart_data_returns_chart_data(self):
+        chart_data = self.normal_chart.get_chart_data()
+        self.assertEqual(self.chart_data, chart_data)
