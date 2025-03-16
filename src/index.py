2fc8c5342ccd1eae95da3d139b49bdafd511af7d
Markus Wahlman <wahlman2002@gmail.com>
2025-03-16 22:12:57 +0200

    Add index.py basic functionality for manual testing

--- src/index.py
+++ src/index.py
@@ -1,5 +1,17 @@
+from blackjack_helper import BlackJackHelper
+from chart import Chart
+import json
+
+
 def main():
-    print("Ben")
+    files = ["data/single_deck_so17_normal.json", "data/single_deck_so17_soft.json", "data/single_deck_so17_split.json"]
+    normal_chart, soft_chart, split_chart = [Chart(json.load(open(file, "r"))) for file in files]
+
+    blackjackHelper = BlackJackHelper(
+        normal_chart, soft_chart, split_chart
+    )
+
+    print(blackjackHelper.ask_help("10", ["5", "6"]))
 
 
 if __name__ == "__main__":
