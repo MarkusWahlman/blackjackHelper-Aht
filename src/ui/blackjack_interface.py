2ffbccbd1a5c7528ebd4e3bddeb5909036c894e7
Markus Wahlman <wahlman2002@gmail.com>
2025-04-01 21:39:19 +0300

    Fix card images

--- src/ui/blackjack_interface.py
+++ src/ui/blackjack_interface.py
@@ -11,6 +11,7 @@ class BlackjackInterface:
 
     def _update_dealer_card(self, _, app_data):
         self.dealer_card = app_data
+        dpg.configure_item(self.dealer_card_image, texture_tag=self.card_textures.get(app_data, ""))
         self._update_help_text()
 
     def _update_player_cards(self, _, app_data, card_index):
@@ -18,6 +19,7 @@ class BlackjackInterface:
             raise IndexError("Invalid card index")
         else:
             self.player_cards[card_index] = app_data
+            dpg.configure_item(self.player_cards_images[card_index], texture_tag=self.card_textures.get(self.player_cards[card_index], ""))
         self._update_help_text()
 
     def _add_player_card(self):
@@ -33,15 +35,18 @@ class BlackjackInterface:
 
     def _update_card_listboxes(self):
         dpg.set_value(self.dealer_card_listbox, self.dealer_card)
-
+        
         for card_index in range(self.max_player_cards):
             should_show = card_index < min(
                 len(self.player_cards), self.max_player_cards)
-            dpg.configure_item(
-                self.player_cards_listboxes[card_index], show=should_show)
+            
+            dpg.configure_item(self.player_cards_listboxes[card_index], show=should_show)
+            dpg.configure_item(self.player_cards_images[card_index], show=should_show)
+
             if should_show:
-                dpg.set_value(
-                    self.player_cards_listboxes[card_index], self.player_cards[card_index])
+                dpg.set_value(self.player_cards_listboxes[card_index], self.player_cards[card_index])
+                dpg.configure_item(self.player_cards_images[card_index], texture_tag=self.card_textures.get(self.player_cards[card_index], ""))
+                dpg.configure_item(self.dealer_card_image, texture_tag=self.card_textures.get(self.dealer_card, ""))
 
     def _reset_game(self):
         self.dealer_card = self.DEFAULT_CARD
@@ -50,18 +55,22 @@ class BlackjackInterface:
         self._update_help_text()
         self._update_card_listboxes()
 
-    def _add_card_listbox(self, callback, user_data=None, show_condition=True):
-        card_listbox = dpg.add_listbox(
-            width=20,
-            items=BLACKJACK_CARDS,
-            num_items=10,
-            callback=callback,
-            user_data=user_data,
-            show=show_condition
-        )
-        # dpg.add_image(self.texture_tag)
+    def _add_card_listbox_and_image(self, callback, user_data=None, show_condition=True):
+        with dpg.group(horizontal=True):
+            card_listbox = dpg.add_listbox(
+                width=20,
+                items=BLACKJACK_CARDS,
+                num_items=10,
+                callback=callback,
+                user_data=user_data,
+                show=show_condition
+            )
+            
+            texture_tag = self.card_textures.get(self.DEFAULT_CARD, "")
+            scale = 0.8
+            card_image = dpg.add_image(texture_tag, width=int(dpg.get_item_width(texture_tag) * scale), height=int(dpg.get_item_height(texture_tag) * scale), show=show_condition)
 
-        return card_listbox
+        return card_listbox, card_image
     
     def _change_rule(self, _, app_data, user_data):
         self.blackjack_helper.set_rule(user_data, app_data)
@@ -75,7 +84,7 @@ class BlackjackInterface:
         with dpg.window() as blackjack_window:
             dpg.add_button(label="Settings", callback=self._show_settings)
 
-            self.dealer_card_listbox = self._add_card_listbox(
+            self.dealer_card_listbox, self.dealer_card_image  = self._add_card_listbox_and_image(
                 self._update_dealer_card)
             
             total_rows = math.ceil(self.max_player_cards / self.cards_per_row)
@@ -86,8 +95,9 @@ class BlackjackInterface:
                         if card_index < self.max_player_cards:
                             show_condition = card_index < min(
                                 len(self.player_cards), self.max_player_cards)
-                            self.player_cards_listboxes.append(self._add_card_listbox(
-                                self._update_player_cards, card_index, show_condition))
+                            listbox, image = self._add_card_listbox_and_image(self._update_player_cards, card_index, show_condition)
+                            self.player_cards_listboxes.append(listbox)
+                            self.player_cards_images.append(image)
 
             dpg.add_button(label="Add new card",
                            callback=self._add_player_card)
@@ -117,12 +127,17 @@ class BlackjackInterface:
         
 
     def setup_ui(self):
-        # width, height, _, data = dpg.load_image("data/images/1.png")
+        self.card_textures = {}
 
-        # with dpg.texture_registry():
-        #    self.texture_tag = dpg.add_static_texture(width=width, height=height, default_value=data)
+        for card in BLACKJACK_CARDS:
+            image_path = f"data/images/{card}.png"
+            width, height, _, data = dpg.load_image(image_path)
 
-        dpg.create_viewport(title='Blackjack Helper', width=600, height=500)
+            with dpg.texture_registry():
+                texture_tag = dpg.add_static_texture(width=width, height=height, default_value=data)
+                self.card_textures[card] = texture_tag
+
+        dpg.create_viewport(title='Blackjack Helper', width=800, height=520)
 
         self._setup_settings_window()
         self._setup_primary_window()
@@ -133,6 +148,7 @@ class BlackjackInterface:
         self.dealer_card = self.DEFAULT_CARD
         self.player_cards = [self.DEFAULT_CARD, self.DEFAULT_CARD]
         self.player_cards_listboxes = []
+        self.player_cards_images = []
 
         files = ["data/charts/single_deck_so17_normal.json",
                  "data/charts/single_deck_so17_soft.json", "data/charts/single_deck_so17_split.json"]
